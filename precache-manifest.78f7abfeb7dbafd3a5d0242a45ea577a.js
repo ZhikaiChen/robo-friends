55ae78bd4d7b1bf84034771dac01eef63eb7d7ba
Zhikai Chen <chen.zhikai@wustl.edu>
2020-01-18 19:47:01 -0600

    Updates

---
+++ precache-manifest.78f7abfeb7dbafd3a5d0242a45ea577a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea6fc3c7bd5ab20818719f4b2b9f086",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "6401222e18d4e6a18b6c",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d32d1b9e56c23f373652",
+    "url": "/robo-friends/static/css/main.00be0d19.chunk.css"
+  },
+  {
+    "revision": "6401222e18d4e6a18b6c",
+    "url": "/robo-friends/static/js/2.28ed544f.chunk.js"
+  },
+  {
+    "revision": "d32d1b9e56c23f373652",
+    "url": "/robo-friends/static/js/main.23c4c4d6.chunk.js"
+  },
+  {
+    "revision": "4495fefb041e0d2f6e17",
+    "url": "/robo-friends/static/js/runtime-main.7eea84bc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
